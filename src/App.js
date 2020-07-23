d2bbfb333e69879d5cef6bc105b0b2fdc2aec938
sauldesigns <splat002@ucr.edu>
2020-07-22 19:59:50 -0700

    added react router, and added basic routes, and organized code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import './App.css';
-import Row from './components/Row';
-import Banner from './components/Banner';
-import Nav from './components/Nav';
-import requests from './requests';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Homepage from './screens/homepage/Homepage';
+import Login from './screens/login/Login';
+import * as ROUTES from './constants/routes';
 
 function App() {
 	return (
-		<div className='app'>
-			<Nav />
-			<Banner />
-			<Row
-				isLargeRow
-				title='NETFLIX ORIGINALS'
-				fetchUrl={requests.fetchNetflixOriginal}
-			/>
-			<Row title='Trending Now' fetchUrl={requests.fetchTrending} />
-			<Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
-			<Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
-			<Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
-			<Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
-			<Row
-				title='Documentaries Movies'
-				fetchUrl={requests.fetchDocumentaries}
-			/>
-		</div>
+		<Router>
+			<Switch>
+				<Route path={ROUTES.LOGIN} component={Login} />
+				<Route path={ROUTES.HOME} component={Homepage} />
+				<Route path={ROUTES.LANDING} component={Homepage} />
+			</Switch>
+		</Router>
 	);
 }
 
---
+++ src/screens/Homepage/Homepage.js
@@ -0,0 +1,31 @@
+import React from './node_modules/react';
+import './Homepage.css';
+import Row from '../../components/Row';
+import Banner from '../../components/Banner';
+import Nav from '../../components/Nav';
+import requests from '../../requests';
+
+function Homepage() {
+	return (
+		<div className='app'>
+			<Nav />
+			<Banner />
+			<Row
+				isLargeRow
+				title='NETFLIX ORIGINALS'
+				fetchUrl={requests.fetchNetflixOriginal}
+			/>
+			<Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+			<Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+			<Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+			<Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+			<Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+			<Row
+				title='Documentaries Movies'
+				fetchUrl={requests.fetchDocumentaries}
+			/>
+		</div>
+	);
+}
+
+export default Homepage;
