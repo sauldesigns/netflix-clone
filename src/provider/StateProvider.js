1da321a93c12768ef06981845c90282d1fb82fb0
sauldesigns <splat002@ucr.edu>
2020-07-22 20:25:21 -0700

    setup basic react context api

---
+++ src/provider/StateProvider.js
@@ -0,0 +1,9 @@
+import React, { createContext, useReducer } from 'react';
+
+export const StateContext = createContext();
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+	<StateContext.Provider value={useReducer(reducer, initialState)}>
+		{children}
+	</StateContext.Provider>
+);
